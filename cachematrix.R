13d75cd1981f8ae7ce220bbad446019fb91449a7
xkata <katazkataz@gmail.com>
2015-04-26 17:24:04 +0200

    makecache func changed

--- cachematrix.R
+++ cachematrix.R
@@ -2,9 +2,18 @@
 ## functions do
 
 ## Write a short comment describing this function
-
+## To S
 makeCacheMatrix <- function(x = matrix()) {
-
+        inverse <- NULL
+        set <-function(y) {
+              x<<- y
+              inverse <<-NULL
+        }
+        
+        get <-function() x
+        setinverse <- function(theinverse) inverse <<- theinverse
+        getinverse <- function() inverse
+        list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
 }
 
 
